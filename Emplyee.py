e6fd684182b7941f9778faf389d1d9433abe7ea4
wakibd-glitch <wakibd@gmail.com>
2026-02-18 16:46:37 +1100

    Add files via upload

---
+++ Emplyee.py
@@ -0,0 +1,20 @@
+class Person( object ):	
+		
+		def __init__(self, name, idnumber):
+				self.name = name
+				self.idnumber = idnumber
+		def display(self):
+				print(self.name)
+				print(self.idnumber)
+
+class Employee( Person ):		
+		def __init__(self, name, idnumber, salary, post):
+				self.salary = salary
+				self.post = post
+
+				
+				Person.__init__(self, name, idnumber)
+
+
+Clark = Employee("john", 12, 299999, "Banana Road")
+Clark.display()
